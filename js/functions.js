8c29986f875b7a2416d92d7f47f5da1db7f7e14e
EnesT51 <99040669@mydavinci.nl>
2023-04-19 11:06:57 +0200

    gave functionality to the price filter button and also added a regex function to check if the input is a valid int or not

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,6 @@
 let Container = document.getElementById('container');
-    
+let WinkelMandje = document.getElementById('winkelmandje');
+
 let label = document.createElement('label');
 label.innerHTML = 'Choose Genre:';
 label.id = 'label';
@@ -8,6 +9,7 @@ Container.appendChild(label);
 let DropdownList = document.createElement('select');
 DropdownList.id = 'DropDownList';
 DropdownList.options[0] = new Option('--Select Genre--', '');
+DropdownList.options[1] = new Option('Show All', '');
 Container.appendChild(DropdownList);
 
 let OkButton = document.createElement('button');
@@ -17,7 +19,8 @@ Container.appendChild(OkButton);
 
 let InputField = document.createElement('input');
 InputField.id = 'InputField';
-InputField.setAttribute('type', 'text');
+InputField.placeholder = 'Price';
+InputField.setAttribute('type', 'number');
 Container.appendChild(InputField);
 
 let SecondOkButton = document.createElement('button');
@@ -25,8 +28,24 @@ SecondOkButton.id = 'SecondOkButton';
 SecondOkButton.innerHTML = 'Filter';
 Container.appendChild(SecondOkButton);
 
+let CalculateBtn = document.createElement('button');
+CalculateBtn.innerHTML = 'Bereken';
+CalculateBtn.id = 'CalculateBtn';
+
 let GameContentDiv = document.createElement('div');
 GameContentDiv.id = "GameContent";
+
+function AppendData(content, item) {
+    let ul = document.createElement('ul');
+    let GameName = document.createElement('li');
+    let GamePrice = document.createElement('li');
+    GamePrice.id = 'GamePrice';
+    GamePrice.innerHTML = "Price: € " + item.price +',-';
+    GameName.innerHTML = item.title;
+    ul.appendChild(GameName);
+    ul.appendChild(GamePrice);
+    content.appendChild(ul);
+}
 
 async function GetData(){
     let GameList = [];
@@ -52,7 +71,6 @@ async function DisplayDataInDropDownList(data){
         if(!genrelist.includes(x.genre)){
             genrelist.push(x.genre);
         }
-        
     }
     for(const i of genrelist){
         let option = document.createElement('option');
@@ -60,29 +78,24 @@ async function DisplayDataInDropDownList(data){
         DropdownList.add(option);
     }
 }
-
 async function DisplayGames(data){
 
     const json = await data;
 
     if(!json.length == 0){
         for(const i of json){
-            let ul = document.createElement('ul');
-            let GameName = document.createElement('li');
-            let GamePrice = document.createElement('li');
-            GamePrice.id = 'GamePrice';
-            GamePrice.innerHTML = "Price: € " + i.price +',-';
-            GameName.innerHTML = i.title;
-            ul.appendChild(GameName);
-            ul.appendChild(GamePrice);
-            GameContentDiv.appendChild(ul);
+            AppendData(GameContentDiv, i)
         }
     }else{
         console.log('List is empty');
     }
     Container.appendChild(GameContentDiv);
+    Container.appendChild(CalculateBtn);
 }
+function CheckIfInputIfDigit(input){
 
+    return /^[0-9+$]/.test(input.value);
+}
 async function FilterGamesByGenre(data){
     
     const json = await data;
@@ -92,25 +105,35 @@ async function FilterGamesByGenre(data){
 
     for(const i of result){
         if(!json.length == 0){
-            let ul = document.createElement('ul');
-            let GameName = document.createElement('li');
-            let GamePrice = document.createElement('li');
-            GamePrice.id = 'GamePrice';
-            GamePrice.innerHTML = "Price: € " + i.price +',-';
-            GameName.innerHTML = i.title;
-            ul.appendChild(GameName);
-            ul.appendChild(GamePrice);
-            GameContentDiv.appendChild(ul);
+            AppendData(GameContentDiv, i)
         }
+    }if(text === 'Show All'){
+        DisplayGames(data);
     }
+
     return result;
 }
+
+OkButton.addEventListener("click", () => {
+
+    FilterGamesByGenre(Data);
+});
+
+SecondOkButton.addEventListener("click", async () => {
+    if(CheckIfInputIfDigit(InputField) === true){
+        const filterByPrice = FilterGamesByGenre(Data);
+        const json = await filterByPrice;
+        const result = json.filter(obj => obj.price < InputField.value);
+        GameContentDiv.innerHTML = '';
+        for(const i of result){
+            if(!json.length == 0){
+                AppendData(GameContentDiv, i)
+            }
+        }
+    }
+});
+
 
 let Data = GetData();
 DisplayDataInDropDownList(Data);
 DisplayGames(Data);
-
-OkButton.addEventListener("click", () => {
-
-    let FiltertGames = FilterGamesByGenre(Data);
-});
